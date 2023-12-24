b3ce673517d80770f765200ae7596f63e73c217c
Rafi Soemarno <soemarnorafi@gmail.com>
2023-12-24 14:50:21 +0700

    button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,90 +21,6 @@
 	#hero-button {
 		@apply font-semibold text-center;
 	}
-	#hero-button::after {
-		@apply transition-all duration-700;
-		content: '';
-		opacity: 0;
-		position: absolute;
-		z-index: -1;
-		filter: blur(25px);
-		top: -3px;
-		left: -3px;
-		bottom: -3px;
-		right: -3px;
-		border-radius: 1.25rem;
-		background: conic-gradient(
-			from var(--a) at 50% 50%,
-			#e8515c 0deg,
-			#fe6ebd 45deg,
-			#5cadfe 90deg,
-			#75f5d8 150deg,
-			#fdf175 225deg,
-			#fe8564 290deg,
-			#e8515c 1turn
-		);
-		will-change: auto;
-		animation: a 1.5s linear infinite;
-	}
-	#hero-button:hover::after {
-		opacity: 1;
-	}
-	@property --a {
-		syntax: '<angle>';
-		inherits: false;
-		initial-value: 0deg;
-		will-change: auto;
-	}
-	@keyframes a {
-		from {
-			--a: 0deg;
-		}
-		to {
-			--a: 360deg;
-		}
-	}
-	#hero-button::after {
-		@apply transition-all duration-700;
-		content: '';
-		opacity: 0;
-		position: absolute;
-		z-index: -1;
-		filter: blur(25px);
-		top: -3px;
-		left: -3px;
-		bottom: -3px;
-		right: -3px;
-		border-radius: 1.25rem;
-		background: conic-gradient(
-			from var(--a) at 50% 50%,
-			#e8515c 0deg,
-			#fe6ebd 45deg,
-			#5cadfe 90deg,
-			#75f5d8 150deg,
-			#fdf175 225deg,
-			#fe8564 290deg,
-			#e8515c 1turn
-		);
-		will-change: auto;
-		animation: a 1.5s linear infinite;
-	}
-	#hero-button:hover::after {
-		opacity: 1;
-	}
-	@property --a {
-		syntax: '<angle>';
-		inherits: false;
-		initial-value: 0deg;
-		will-change: auto;
-	}
-	@keyframes a {
-		from {
-			--a: 0deg;
-		}
-		to {
-			--a: 360deg;
-		}
-	}
 	#hero-header {
 		@apply text-6xl text-zinc-100 overflow-hidden;
 	}
@@ -112,7 +28,49 @@
 		@apply relative text-4xl w-fit text-zinc-200 mx-auto p-4 rounded-2xl border-4 border-zinc-200 transition-all duration-500;
 	}
 	#hero-button:hover {
-		@apply text-zinc-900 bg-zinc-200;
+		@apply text-transparent bg-zinc-200;
+	}
+	#hero-button::after {
+		@apply transition-all duration-700;
+		content: '';
+		opacity: 0;
+		position: absolute;
+		z-index: -1;
+		filter: blur(25px);
+		top: -3px;
+		left: -3px;
+		bottom: -3px;
+		right: -3px;
+		border-radius: 1.25rem;
+		background: conic-gradient(
+			from var(--a) at 50% 50%,
+			#e8515c 0deg,
+			#fe6ebd 45deg,
+			#5cadfe 90deg,
+			#75f5d8 150deg,
+			#fdf175 225deg,
+			#fe8564 290deg,
+			#e8515c 1turn
+		);
+		will-change: auto;
+		animation: a 1.5s linear infinite;
+	}
+	#hero-button:hover::after {
+		opacity: 1;
+	}
+	@property --a {
+		syntax: '<angle>';
+		inherits: false;
+		initial-value: 0deg;
+		will-change: auto;
+	}
+	@keyframes a {
+		from {
+			--a: 0deg;
+		}
+		to {
+			--a: 360deg;
+		}
 	}
 	#hero-header > h1 {
 		@apply transition-all duration-700;
